13c0e57d90686aece951f926c42de8dfb6c8bf63
kharismajd-gojek <made.j.interns@aux.gojek.com>
2021-09-22 11:59:28 +0800

    Successfuly calculate the length of two point with one same attribute

--- lib/src/main/java/com/geometry/Point.java
+++ lib/src/main/java/com/geometry/Point.java
@@ -1,8 +1,18 @@
 package com.geometry;
 
 public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     public int distanceTo(Point other) {
-        return 0;
+        if (this.x == other.x) {
+            return Math.abs(this.y - other.y);
+        }
+        return Math.abs(this.x - other.x);
     }
 }
--- lib/src/test/java/com/geometry/PointTest.java
+++ lib/src/test/java/com/geometry/PointTest.java
@@ -8,8 +8,15 @@ public class PointTest {
 
     @Test
     public void twoSamePointsShouldReturnZero() {
-        Point point1 = new Point();
-        Point point2 = new Point();
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(0, 0);
         assertEquals(0, point1.distanceTo(point2), 0.0001);
+    }
+
+    @Test
+    public void twoPointsWithOneSameAttributeShouldReturnResult() {
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(0, 5);
+        assertEquals(5, point1.distanceTo(point2), 0.0001);
     }
 }
